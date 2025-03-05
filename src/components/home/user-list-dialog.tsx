c4e610c62331f3ff894d35690acf60f48a2cb7a2
Bhimadolu-Balu <bhimadolubalu7@gmail.com>
2025-03-05 18:59:13 +0530

    kickUser mutation was added

--- src/components/home/user-list-dialog.tsx
+++ src/components/home/user-list-dialog.tsx
@@ -183,7 +183,7 @@ const UserListDialog = () => {
 							<div className='w-5 h-5 border-t-2 border-b-2  rounded-full animate-spin' />
 						) : (
 							"Create"
-						)}
+						)}q
 					</Button>
 				</div>
 			</DialogContent>
